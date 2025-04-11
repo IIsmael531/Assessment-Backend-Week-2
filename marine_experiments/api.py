4afc20894110dbee240315775480195bd3e043f1
IIsmael531 <ibrahimismael531@gmail.com>
2025-04-11 17:29:26 +0100

    I can't seem to fix the % sign

--- marine_experiments/api.py
+++ marine_experiments/api.py
@@ -65,27 +65,6 @@ def endpoint_get_experiment():
     return get_experiment(conn, type, score_over)
 
 
-# Task 3
-# A GET request to the / experiment endpoint should accept
-# two optional query parameters.
-
-# Both parameters accept only specific values
-# invalid values should result in a 400 response with a JSON response object of the format {"error": "Invalid value for 'x' parameter"}.
-# If both parameters are passed at once, their effects should combine.
-# By default, without the arguments, the endpoint should return a full list of experiments.
-
-
-# type
-# This parameter should accept only "intelligence", "obedience" or "aggression"
-# as values(not case-sensitive). When a valid value is passed to the type parameter,
-# only experiments of that type should be returned.
-
-# score_over
-# This parameter should accept only integer values in the range 0-100.
-# When a valid value is passed to the score_over parameter, only experiments
-# where the percentage score was greater than the value should be returned.
-
-
 if __name__ == "__main__":
     app.config["DEBUG"] = True
     app.config["TESTING"] = True
